4216b8a1dd46c9f3dc17b77a7dd7dbff915254c8
TuurVanBergen <tuur.van.bergen@student.ehb.be>
2023-12-22 20:30:05 +0100

    added comments

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express= require('express');
 const app = express();
 const cors = require('cors');
 const {MongoClient} = require('mongodb');
-const {v4: uuidv4, validate: uuidValidate} = require('uuid')
-const { ObjectId } = require('mongodb');
+const {v4: uuidv4} = require('uuid')
 
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
@@ -12,10 +11,10 @@ app.use(express.json())
 app.listen(3000);
 console.log("app running at http://localhost:3000");
 
-async function getDatabase() {                                                                                                                                      
+async function getDatabase() {
+                                                                                                                                          
     const url = "mongodb+srv://tuurvanbergen:Tuur.1234@web2.mjdzevf.mongodb.net/?retryWrites=true&w=majority";
-
-    // Connect to your Atlas cluster
+    // Connect to Atlas
     const client = new MongoClient(url);
 
     await client.connect();
@@ -24,13 +23,7 @@ async function getDatabase() {
     return client.db('brussel_in_beeld');
 }
 
-
-
-//create monogclient
-//const client = new MongoClient("mongodb+srv://tuurvanbergen:Tuur.1234@web2.mjdzevf.mongodb.net/?retryWrites=true&w=majority")
-//let users = [];
-
-
+// /Artist Get route, this route gets the data out of the db, searches for a sepcific document. after that it sends a response wit that document.
 app.get("/Artist", async(req,res)=>{
     let database;
     try{
@@ -42,6 +35,8 @@ app.get("/Artist", async(req,res)=>{
 
         //send back the data with response
         res.status(200).send(artist);
+
+        //catch possible errors
     }catch(error){
         console.log(error)
         res.status(500).send({
@@ -49,10 +44,12 @@ app.get("/Artist", async(req,res)=>{
             value: error
         });
     }finally{       
+        //close connection
         await database.client.close();
     }
 })
 
+// /Table-of-contents Get route, this route gets the data out of the db, searches for a sepcific document. after that it sends a response wit that document.
 app.get("/Table-of-contents", async(req,res)=>{
     let database;
     try{
@@ -70,15 +67,13 @@ app.get("/Table-of-contents", async(req,res)=>{
             error: 'Something went wrong!',
             value: error
         });
-    }finally{       
-    await database.client.close();
+    }finally{  
+        //close connection     
+        await database.client.close();
     }
 })
 
-// app.get("/Table-of-contents/:id",async(req,res)=>{
-
-// } )
-
+// /Artwork Get route, this route gets the data out of the db, searches for a sepcific document. after that it sends a response wit that document.
 app.get("/Artwork", async(req,res)=>{
     let database;
     try{
@@ -97,26 +92,34 @@ app.get("/Artwork", async(req,res)=>{
             value: error
         });
     }finally{
+        //close connection 
         await database.client.close();
     }
 })
 
+// /addGallery post route, this wil add a new gallery to the database.
 app.post('/addGallery', async (request, response) => {
     let database;
+
     try {
         database = await getDatabase();
+
+        //put all the galleries inisde galleriesCollection
         const galleriesCollection = await database.collection('galleries');
 
+        //This if statement ensures that there can only be a maximum of 10 documents.
         if(await galleriesCollection.countDocuments() >=10){
             return response.status(400).send({ error: 'Maximum galleries reached.' });
         }
 
         const existingGallery = await galleriesCollection.findOne({ title: request.body.title });
 
+        //this if statement ensures that there are no empty fields.
         if (!request.body.title || !request.body.artist || !request.body.opening_hours || !request.body.price || !request.body.location || !request.body.accesability || !request.body.facebook_link || !request.body.instagram_link || !request.body.website_link || !request.body.text_1_title || !request.body.text_1 || !request.body.text_2_title || !request.body.text_2 || !request.body.category) {
             return response.status(400).send({ error: 'Vul elk veld in.' });
         }
 
+        //ensures that there are no duplicates
         if (existingGallery) {
             return response.status(400).send({ error: 'Gallery exists already' });
         }
@@ -124,6 +127,7 @@ app.post('/addGallery', async (request, response) => {
         const newGallery = request.body;
         newGallery.uuid = uuidv4();
 
+        //insert a new gallery
         await galleriesCollection.insertOne(newGallery);
 
         return response.send({ message: 'Gallery added successfully!' });
@@ -138,8 +142,7 @@ app.post('/addGallery', async (request, response) => {
     }
 });
 
-
-
+// /deleteGallery delete route, this wil delete a gallery.
 app.delete('/deleteGallery', async (req, res) => {
     let database;
     try {
@@ -154,13 +157,12 @@ app.delete('/deleteGallery', async (req, res) => {
        const existingGallery = await galleriesCollection.findOne({ title: deleteGallery });
 
         if (existingGallery) {
-            // If the document with the given _id exists, you can delete it
+            // If there is already a document with the same name.
           const result = await galleriesCollection.deleteOne({ title: deleteGallery });
            if (result.deletedCount == 1) {
             // Document successfully deleted
                res.status(200).send({ message: 'Gallery successfully deleted' });
             } else {
-                 // Delete operation did not affect any documents
                 res.status(404).send({ message: 'Gallery not found' });
             }
          } else {
@@ -175,15 +177,16 @@ app.delete('/deleteGallery', async (req, res) => {
     }
 });
 
+// /updateGallery put route, this wil update a gallery.
 app.put('/updateGallery', async(req, res) => {
     let database;
     try {
         database = await getDatabase();
 
+        //this if statement checks for empty fields.
         if (!req.body.title || !req.body.price || !req.body.artist || !req.body.adres || !req.body.url || !req.body.Iurl || !req.body.Furl) {
             return res.send({ error: 'Missing title, price, artist, adres, url, Iurl or Furl' }).status(400);
         }
-
 
         const updateGallery = {
             title:req.body.title,
@@ -196,13 +199,11 @@ app.put('/updateGallery', async(req, res) => {
         }
         const galleriesCollection = await database.collection('galleries');
 
-
-
         // Find the document using the title field
         const existingGallery = await galleriesCollection.findOne({ title: updateGallery.title });
 
+        //$set will, update the existing fields with new data.
         if (existingGallery) {
-           // updateOne()
            const result = await galleriesCollection.updateOne(
             { title: updateGallery.title },
             {
@@ -220,7 +221,6 @@ app.put('/updateGallery', async(req, res) => {
             // Document successfully updated
             res.status(200).send({ message: 'Gallery was updated succesfully' });
            } else {
-                  // Update operation did not affect any documents
                  res.status(404).send({ message: 'Gallery not found' });
             }
         } else {
@@ -231,6 +231,7 @@ app.put('/updateGallery', async(req, res) => {
         console.error('Error:', error);
         res.status(500).send({ message: 'Internal server error' });
     } finally {
+        //close the connection
         await database.client.close();
     }
 })
